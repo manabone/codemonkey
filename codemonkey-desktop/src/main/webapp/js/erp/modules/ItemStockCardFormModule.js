5ce4c6d56b024d73ca4123051aea1f18f7c4214c
manabone <zb0413@gmail.com>
2013-04-16 11:05:30 +0800

    item stock card

--- codemonkey-desktop/src/main/webapp/js/erp/modules/ItemStockCardFormModule.js
+++ codemonkey-desktop/src/main/webapp/js/erp/modules/ItemStockCardFormModule.js
@@ -16,7 +16,21 @@ Ext.define('erp.modules.ItemStockCardFormModule', {
     },
     
     formItems : function(){
-		return [{"xtype":"textfield","name":"code","fieldLabel":"编码"},{"xtype":"textfield","name":"name","fieldLabel":"名称"},{"xtype":"textfield","name":"description","fieldLabel":"描述"},{"xtype":"textfield","name":"createdBy","fieldLabel":"创建人"},{"xtype":"textfield","name":"modifiedBy","fieldLabel":"修改人"},{"xtype":"numberfield","name":"qtyOnHand","fieldLabel":"qty on hand"},{"xtype":"numberfield","name":"id","fieldLabel":"自动编号"},{"xtype":"numberfield","name":"originVersion","fieldLabel":"origin version"},{"xtype":"datefield","name":"creationDate","format":"Y-m-d","fieldLabel":"创建时间"},{"xtype":"datefield","name":"modificationDate","format":"Y-m-d","fieldLabel":"修改时间"},{"xtype":"searchingselect","name":"item","config":{"model":"ItemList"},"fieldLabel":"item"},{"xtype":"searchingselect","name":"warehouse","config":{"model":"WarehouseList"},"fieldLabel":"warehouse"}];
+		//return [{"xtype":"textfield","name":"code","fieldLabel":"编码"},{"xtype":"textfield","name":"name","fieldLabel":"名称"},{"xtype":"textfield","name":"description","fieldLabel":"描述"},{"xtype":"textfield","name":"createdBy","fieldLabel":"创建人"},{"xtype":"textfield","name":"modifiedBy","fieldLabel":"修改人"},{"xtype":"numberfield","name":"qtyOnHand","fieldLabel":"qty on hand"},{"xtype":"numberfield","name":"id","fieldLabel":"自动编号"},{"xtype":"numberfield","name":"originVersion","fieldLabel":"origin version"},{"xtype":"datefield","name":"creationDate","format":"Y-m-d","fieldLabel":"创建时间"},{"xtype":"datefield","name":"modificationDate","format":"Y-m-d","fieldLabel":"修改时间"},{"xtype":"searchingselect","name":"item","config":{"model":"ItemList"},"fieldLabel":"item"},{"xtype":"searchingselect","name":"warehouse","config":{"model":"WarehouseList"},"fieldLabel":"warehouse"}];
+    
+    	var p1 = ExtUtils.panel({
+    		title : 'basic info',
+			items:[
+    			{xtype:"searchingselect",name:"item","config":{"model":"ItemList"},fieldLabel:"item", readOnly : true},
+    			{xtype :"searchingselect",name :"warehouse",config :{model :"WarehouseList"},fieldLabel :"warehouse" , readOnly : true},
+    			{xtype:"numberfield",name:"qtyOnHand",fieldLabel:"qty on hand" , readOnly : true}
+			]
+    	});
+    	
+    	var p2 = ExtUtils.creationInfoPanel();
+    	
+    	return ExtUtils.fitLayout([p1 , p2]);
+    
     },
 	
 	createBbar : function(){
--- codemonkey-desktop/src/main/webapp/js/erp/modules/ItemStockCardListModule.js
+++ codemonkey-desktop/src/main/webapp/js/erp/modules/ItemStockCardListModule.js
@@ -18,10 +18,18 @@ Ext.define('erp.modules.ItemStockCardListModule', {
 	iconCls : 'icon-grid',
     
     modelFields : function(){
-    	return ['item','warehouse','qtyOnHand','id','code','name','description','originVersion','creationDate','createdBy','modificationDate','modifiedBy'];
+    	return ['item' , 'item_text' , 'warehouse' , 'warehouse_text' ,'qtyOnHand'].concat(ExtUtils.defaultModelFields);
+    
     },
 	gridCols :  function(){
-		return [{"dataIndex":"item","flex":1,"header":"item"},{"dataIndex":"warehouse","flex":1,"header":"warehouse"},{"dataIndex":"qtyOnHand","flex":1,"header":"qty on hand"},{"dataIndex":"id","flex":1,"header":"自动编号"},{"dataIndex":"code","flex":1,"header":"编码"},{"dataIndex":"name","flex":1,"header":"名称"},{"dataIndex":"description","flex":1,"header":"描述"},{"dataIndex":"originVersion","flex":1,"header":"origin version"},{"dataIndex":"creationDate","flex":1,"header":"创建时间"},{"dataIndex":"createdBy","flex":1,"header":"创建人"},{"dataIndex":"modificationDate","flex":1,"header":"修改时间"},{"dataIndex":"modifiedBy","flex":1,"header":"修改人"}];
+		
+		return [
+	          {"dataIndex":"item",hidden:true,"header":"item"},
+	          {"dataIndex":"item_text","flex":1,"header":"item"},
+	          {"dataIndex":"warehouse",hidden:true,"header":"warehouse"},
+	          {"dataIndex":"warehouse_text","flex":1,"header":"warehouse"},
+	          {"dataIndex":"qtyOnHand","flex":1,"header":"qty on hand"}
+         ].concat(ExtUtils.defaultGridCols2);
 	},
 	
 	searchForm : function() {
