ec48ff719d65fe11585ee4256ca3f03b3fd3f466
manabone <zb0413@gmail.com>
2013-04-16 10:31:14 +0800

    item stock card

---
+++ codemonkey-erp/src/main/java/com/codemonkey/erp/controller/ItemStockCardFormController.java
@@ -0,0 +1,23 @@
+package com.codemonkey.erp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.codemonkey.erp.domain.ItemStockCard;
+import com.codemonkey.erp.service.ItemStockCardService;
+import com.codemonkey.web.controller.AbsFormExtController;
+
+
+@Controller
+@RequestMapping("/ext/itemStockCard/**")
+public class ItemStockCardFormController extends AbsFormExtController<ItemStockCard>{
+
+	@Autowired private ItemStockCardService stockCardService;
+	
+	@Override
+	protected ItemStockCardService service() {
+		return stockCardService;
+	}
+
+}
